fef5a390e30ae8e7c8b7fd592e339f71fe41a198
Patrick <patrick.bradley@gladstone.ucsf.edu>
2018-03-26 12:33:43 -0700

    added worker function

---
+++ worker.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import os, sys, beanstalkc
+import cgi
+import uuid
+#from subprocess32 import run
+import subprocess
+import beanstalkc
+import re
+
+beanstalk = beanstalkc.Connection(host='localhost', port=14711)
+beanstalk.watch("phylogenize")
+
+while True:
+  job = beanstalk.reserve()
+  print("received job %s" % job.body)
+  job_file_match = re.search("output_dir = \"([^\"]*)\"", job.body)
+  job_file = job_file_match.group(1)
+  with open(os.path.join(job_file, "progress.txt"), 'w') as fh:
+    with open(os.path.join(job_file, "stderr.txt"), 'w') as fh2:
+      p = subprocess.Popen(["Rscript", "-e", job.body],\
+          stdout=fh, \
+          stderr=fh2)
+  job.delete()
