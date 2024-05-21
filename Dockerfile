1b555840885fe84a5d4b3be430e06b482dbd42f4
kathryn Kananen <Kekananen@gmail.com>
2024-05-21 14:26:00 -0400

    Added documentation to figure functions; fixed docker label

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM condaforge/mambaforge
-MAINTAINER kananen.13@osu.edu
+LABEL org.opencontainers.image.authors="kananen.13@osu.edu"
 
 RUN mamba install -y -c bioconda \
 	bioconductor-qvalue \
@@ -10,7 +10,7 @@ RUN mamba install -y -c conda-forge \
 	r-devtools \
 	r-ragg \
 	r-phylolm \
-	r-phangorn	
+	r-phangorn
 
 RUN R -e "devtools::install_bitbucket('pbradz/phylogenize/package/phylogenize')"
 #RUN R -e "devtools::install_github('pbradleylab/phylogenize')"
--- figures/figure-functions.R
+++ figures/figure-functions.R
@@ -1,22 +1,25 @@
+# Returns the last element of a vector
 last_elem <- function(x) x[length(x)]
 
-make_qvs <- function(tbl,
-                     .nestby="phylum",
-                     .pvcol="p.value",
-                     .qvcol="q.value") {
-    .qv <- sym(.qvcol)
-    .pv <- sym(.pvcol)
-    .nb <- sym(.nestby)
+# Calculates q-values for p-values within groups specified by a given variable
+# in a table. Takes a table (tbl) and optional arguments for grouping (.nestby),
+# p-value column name (.pvcol), and q-value column name (.qvcol). Then the table
+# is grouped by the specified variable, data is nested within each group, 
+# and q-values are calcaulted for each. Finally, the dataframe is unnested and
+# the original structure is returned.
+make_qvs <- function(tbl, .nestby = "phylum", .pvcol = "p.value", .qvcol = "q.value") {
     tbl %>%
-        group_by(!!(.nb)) %>%
-        nest %>%
-        mutate(data=map(data, function(x) {
-            mutate(x,
-                   !!(.qv) := phylogenize:::qvals(!!(.pv), error_to_file=FALSE))
-        })) %>%
-        unnest
+        group_by({{ .nestby }}) %>%
+        nest() %>%
+        mutate(data = map(data, ~ mutate(., {{ .qvcol }} := phylogenize:::qvals({{ .pvcol }}, error_to_file = FALSE)))) %>%
+        unnest()
 }
 
+# Calculates equivalent p-values for effect size, standard error, and degrees
+# of freedom within a table. The function takes a table (tbl) and an optional
+# argument for the minimum effect size (mfx). It creates a new column (equiv.pv)
+# using the phylogenize package's equivalent test function, which calculates
+# p-values based on effect size, standard error, and degrees of freedom.
 make_equivs <- function(tbl, mfx=0.5) {
     mutate(tbl,
            equiv.pv = pmap_dbl(list(effect.size, std.err, df),
@@ -24,25 +27,48 @@ make_equivs <- function(tbl, mfx=0.5) {
                                min_fx=mfx))
 }
 
+# Processes data by calculating equivalent p-values, q-values, and adjusted q-values.
+# The function takes a table (tbl) and applies a series of operations: first, it
+# calculates equivalent p-values using make_equivs, then it calculates q-values
+# using make_qvs, and finally, it calculates adjusted q-values using make_qvs
+# again with the equivalent p-values and corresponding q-values
 process_data <- function(tbl) {
-    tbl %>% make_equivs %>% make_qvs %>%
+    tbl %>% 
+        make_equivs %>% 
+        make_qvs %>%
         make_qvs(., .pvcol="equiv.pv", .qvcol="equiv.qv")
 }
 
+# Retrieves the alpha parameter from a phylogenetic tree for a given phylum and
+# gene. The function takes the phylum name (phylum), gene name (gene),
+# phylogenetic tree database (db), and an optional argument for the lab parameter
+# (lab) which sets the upper limit for log(alpha). It extracts the phylogenetic
+# tree corresponding to the specified phylum from the provided database, selects
+# the tips common to the tree and gene presence data, constructs a data frame, and
+# performs a phylogenetic linear regression using the phyloglm function from the
+# ape package. It returns the estimated alpha parameter if successful, otherwise
+# raises a warning and returns NA.
 get_alpha <- function(phylum, gene, db=pz.db, lab=8) {
-                                        #    attr(p, "class") <- "phylo"
     p <- db$trees[[phylum]]
-    tips <- intersect(colnames(db$gene.presence[[phylum]]),
-                      p$tip.label)
+    tips <- intersect(colnames(db$gene.presence[[phylum]]), p$tip.label)
     df <- data.frame(g=db$gene.presence[[phylum]][gene, tips])
-    if (var(df$g) == 0) { warning(paste0("zero-variance: ", gene)); return(NA) }
-    tryCatch({phyloglm(g ~ 1,
-                       data=df,
-                       phy=keep.tip(p, tips),
-                       log.alpha.bound=lab)$alpha },
-             error=function(e) { warning(e); NA })
+    if (var(df$g) == 0) { 
+        warning(paste0("zero-variance: ", gene))
+        return(NA)
+    }
+    tryCatch({
+        phyloglm(g ~ 1,
+                data=df,
+                phy=keep.tip(p, tips),
+                log.alpha.bound=lab)$alpha
+    }, error=function(e) { 
+        warning(e) 
+        NA 
+    })
 }
 
+# Sets the class attribute of an object to "phylo". This is used to mark an object
+# as a phylogenetic tree.
 ap_tree <- function(x) {
     attr(x, "class") <- "phylo"
     x
